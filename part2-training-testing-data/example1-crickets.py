e0b457c4e4efe3ef69bec0d8dddc376ab924f56d
Kristian Chappell <kchappell1@cps.edum>
2023-11-29 11:32:26 -0600

    fixed mistake in part 2

--- part2-training-testing-data/example1-crickets.py
+++ part2-training-testing-data/example1-crickets.py
@@ -10,11 +10,15 @@ from sklearn.model_selection import train_test_split
 
 # imports the data and sets x and y values
 data = pd.read_csv("part2-training-testing-data/chirping_data.csv")
-x = data["Chirps"].values
-y = data["Temp"].values
+x = data["Temp"].values
+y = data["Chirps"].values
 
 # separates the data into training and testing sets
 xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
+print("x: {x}")
+print("y: {y}")
+print("xtrain: {xtrain}")
+print("ytrain: {ytrain}")
 
 # reshape the xtrain data into a 2D array
 xtrain = xtrain.reshape(-1, 1)
@@ -42,6 +46,7 @@ xtest = xtest.reshape(-1,1)
 predict = model.predict(xtest)
 # round the value in the np array to 2 decimal places
 predict = np.around(predict, 2)
+print(predict)
 
 # compare the actual and predicted values
 print("\nTesting Linear Model with Testing Data:")
